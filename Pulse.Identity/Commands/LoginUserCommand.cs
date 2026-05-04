ffb275042ab75b0df7f50ff6ac9dda1c27f56700
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-04 18:42:21 +0200

    login commnd updated

--- Pulse.Identity/Commands/LoginUserCommand.cs
+++ Pulse.Identity/Commands/LoginUserCommand.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
+using Pulse.Identity.DTOs;
 
 namespace Pulse.Identity.Commands;
 
-public record LoginUserCommand(string Email, string Password) : IRequest<string>;
+public record LoginUserCommand(string Email, string Password) : IRequest<AuthResponse>;
