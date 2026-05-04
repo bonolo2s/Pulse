31e7b5756ac54b2ac27f33e296a12d00760679cc
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-04 18:41:23 +0200

    IAuth service updated

--- Pulse.Identity/Interfaces/IAuthService.cs
+++ Pulse.Identity/Interfaces/IAuthService.cs
@@ -1,9 +1,11 @@
-﻿namespace Pulse.Identity.Interfaces;
+﻿using Pulse.Identity.DTOs;
+
+namespace Pulse.Identity.Interfaces;
 
 public interface IAuthService
 {
     Task<User> RegisterUserAsync(User user, string password);
-    Task<string> LoginUserAsync(string email, string password);
+    Task<AuthResponse> LoginUserAsync(string email, string password);
     Task<User> GetCurrentUserAsync(Guid userId);
     Task<User> UpdateUserAsync(Guid userId, User updated);
     Task ChangePasswordAsync(Guid userId, string currentPassword, string newPassword);
