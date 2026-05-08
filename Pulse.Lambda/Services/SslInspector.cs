b654e9421ca97687e1acd15b1f4cefb08c1e6ad3
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-08 14:08:31 +0200

    ssl checker

---
+++ Pulse.Lambda/Services/SslInspector.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Security;
+using System.Net.Sockets;
+
+namespace Pulse.Lambda.Services;
+
+public class SslInspector
+{
+    private const int ExpiryThresholdDays = 7;
+
+    public async Task<(bool IsExpiringSoon, DateTime ExpiryDate)> InspectAsync(string url)
+    {
+        var uri = new Uri(url);
+
+        using var tcpClient = new TcpClient();
+        await tcpClient.ConnectAsync(uri.Host, 443);
+
+        using var sslStream = new SslStream(tcpClient.GetStream());
+        await sslStream.AuthenticateAsClientAsync(uri.Host);
+
+        var certificate = sslStream.RemoteCertificate!;
+        var expiryDate = DateTime.Parse(certificate.GetExpirationDateString());
+        var isExpiringSoon = expiryDate <= DateTime.UtcNow.AddDays(ExpiryThresholdDays);
+
+        return (isExpiringSoon, expiryDate);
+    }
+}
