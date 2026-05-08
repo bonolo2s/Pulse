36a1b072b6d6f814f86f50f538122d3ae82314dc
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-08 14:04:21 +0200

    Lambda's brains defined

---
+++ Pulse.Lambda/Interfaces/IHealthCheckService.cs
@@ -0,0 +1,9 @@
+﻿namespace Pulse.Lambda.Interfaces;
+
+public interface IHealthCheckService
+{
+    Task<bool> PingAsync(string url);
+    Task<long> MeasureLatencyAsync(string url);
+    Task InspectSslAsync(string url);
+    Task ReportResultAsync(Guid endpointId, bool isUp, long latencyMs);
+}
---
+++ Pulse.Lambda/Models/HealthCheckPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Pulse.Lambda.Models;
+
+public class HealthCheckPayload
+{
+    public Guid EndpointId { get; init; }
+    public string Url { get; init; } = string.Empty;
+    public int CheckIntervalSeconds { get; init; }
+}
