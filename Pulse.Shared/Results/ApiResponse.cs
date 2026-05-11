e59616c68e4d2931f604a92d7f099f522547e687
Bonolo Selowa <bonololloyd003@gmail.com>
2026-05-11 05:35:58 +0200

    API Response added to catch exceptions

---
+++ Pulse.Shared/Results/ApiResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Pulse.Shared.Results;
+
+public class ApiResponse<T>
+{
+    public T? Result { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Error { get; set; }
+
+    public static ApiResponse<T> Success(T result, string message = "") => new()
+    {
+        Result = result,
+        Message = message,
+        Error = false
+    };
+
+    public static ApiResponse<T> Failure(string message) => new()
+    {
+        Result = default,
+        Message = message,
+        Error = true
+    };
+}
