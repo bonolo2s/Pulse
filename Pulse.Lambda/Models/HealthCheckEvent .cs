198fe06df04cdf31d42c4b1c00c7e1e184e2191f
Bonolo Selowa <bonololloyd003@gmail.com>
2026-06-12 14:13:47 +0200

    Healthcheck payload updatde

--- Pulse.Lambda/Models/HealthCheckEvent .cs
+++ Pulse.Lambda/Models/HealthCheckEvent .cs
@@ -1,6 +1,9 @@
-﻿namespace Pulse.Lambda.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Pulse.Lambda.Models;
 
 public class HealthCheckEvent
 {
+    [JsonPropertyName("intervalSeconds")]
     public int IntervalSeconds { get; init; }
 }
