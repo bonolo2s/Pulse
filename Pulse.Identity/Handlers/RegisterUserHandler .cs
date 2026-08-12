64345390e940bacd308a95854d245a40da5972bd
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-12 18:31:41 +0200

    add publish from RegisterUserHandler

--- Pulse.Identity/Handlers/RegisterUserHandler .cs
+++ Pulse.Identity/Handlers/RegisterUserHandler .cs
@@ -1,20 +1,27 @@
 ﻿using MediatR;
 using Pulse.Identity.Commands;
 using Pulse.Identity.Interfaces;
+using Pulse.Shared.Events;
 
 namespace Pulse.Identity.Handlers;
 
 public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, User>
 {
     private readonly IAuthService _authService;
+    private readonly IMediator _mediator;
 
-    public RegisterUserHandler(IAuthService authService)
+    public RegisterUserHandler(IAuthService authService, IMediator mediator)
     {
         _authService = authService;
+        _mediator = mediator;
     }
 
     public async Task<User> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
-        return await _authService.RegisterUserAsync(request.User, request.Password);
+        var user = await _authService.RegisterUserAsync(request.User, request.Password);
+
+        await _mediator.Publish(new UserRegisteredNotification(user.Id, user.Email, user.FullName), cancellationToken);
+
+        return user;
     }
 }
