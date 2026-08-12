d34df493aa499870ff13808e94492b21bd1d51e0
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-12 18:50:46 +0200

    remove event driven userSubscription creation

--- Pulse.Identity/Handlers/RegisterUserHandler .cs
+++ Pulse.Identity/Handlers/RegisterUserHandler .cs
@@ -1,27 +1,20 @@
 ﻿using MediatR;
 using Pulse.Identity.Commands;
 using Pulse.Identity.Interfaces;
-using Pulse.Shared.Events;
 
 namespace Pulse.Identity.Handlers;
 
 public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, User>
 {
     private readonly IAuthService _authService;
-    private readonly IMediator _mediator;
 
-    public RegisterUserHandler(IAuthService authService, IMediator mediator)
+    public RegisterUserHandler(IAuthService authService)
     {
         _authService = authService;
-        _mediator = mediator;
     }
 
     public async Task<User> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
-        var user = await _authService.RegisterUserAsync(request.User, request.Password);
-
-        await _mediator.Publish(new UserRegisteredNotification(user.Id, user.Email, user.FullName), cancellationToken);
-
-        return user;
+        return await _authService.RegisterUserAsync(request.User, request.Password);
     }
 }
