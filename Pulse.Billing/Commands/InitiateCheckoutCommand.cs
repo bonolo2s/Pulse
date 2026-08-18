8537cbc4a22d55f17a4974a00474b559afeb562a
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-18 15:11:39 +0200

    initiateCheckout command

---
+++ Pulse.Billing/Commands/InitiateCheckoutCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Pulse.Billing.Payments.Paystack.DTOs;
+
+namespace Pulse.Billing.Commands;
+
+public record InitiateCheckoutCommand(Guid UserId, string Email) : IRequest<InitializeTransactionResult>;
