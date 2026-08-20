56c00eecf272b39de5f5e71a4f885863b1703d86
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-20 21:12:16 +0200

    DB migration removes endpoint limit from subcscription

---
+++ Pulse.Billing/Migrations/20260820190247_RemoveEndpointLimitColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pulse.Billing.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEndpointLimitColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndpointLimit",
+                table: "Subscriptions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EndpointLimit",
+                table: "Subscriptions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
