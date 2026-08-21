caaac87cce2bed9c5e4e28411595cedc5de88d22
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 18:39:53 +0200

    deletePayment n setdefaultPayment command

---
+++ Pulse.Billing/Commands/DeletePaymentMethodCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Pulse.Billing.Commands;
+
+public record DeletePaymentMethodCommand(Guid Id) : IRequest;
---
+++ Pulse.Billing/Commands/SetDefaultPaymentMethodCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Pulse.Billing.Commands;
+
+public record SetDefaultPaymentMethodCommand(Guid Id) : IRequest;
