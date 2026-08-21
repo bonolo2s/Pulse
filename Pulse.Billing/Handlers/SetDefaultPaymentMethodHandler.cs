093e93725761be96c3ad052db427fad050afd743
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 18:42:00 +0200

    SetDefaultPaymentMethodHandler

---
+++ Pulse.Billing/Handlers/SetDefaultPaymentMethodHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Pulse.Billing.Commands;
+using Pulse.Billing.Interfaces;
+
+namespace Pulse.Billing.Handlers;
+
+public class SetDefaultPaymentMethodHandler : IRequestHandler<SetDefaultPaymentMethodCommand>
+{
+    private readonly IPaymentMethodService _paymentMethodService;
+
+    public SetDefaultPaymentMethodHandler(IPaymentMethodService paymentMethodService)
+    {
+        _paymentMethodService = paymentMethodService;
+    }
+
+    public async Task Handle(SetDefaultPaymentMethodCommand request, CancellationToken cancellationToken)
+    {
+        await _paymentMethodService.SetDefaultPaymentMethodAsync(request.Id);
+    }
+}
