52885beb461a09af1abd21e77fd2f3b775f7e9ed
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 18:40:53 +0200

    GetPaymentMethodsHandler

---
+++ Pulse.Billing/Handlers/GetPaymentMethodsHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Pulse.Billing.Entities;
+using Pulse.Billing.Interfaces;
+using Pulse.Billing.Queries;
+
+namespace Pulse.Billing.Handlers;
+
+public class GetPaymentMethodsHandler : IRequestHandler<GetPaymentMethodsQuery, IEnumerable<PaymentMethod>>
+{
+    private readonly IPaymentMethodService _paymentMethodService;
+
+    public GetPaymentMethodsHandler(IPaymentMethodService paymentMethodService)
+    {
+        _paymentMethodService = paymentMethodService;
+    }
+
+    public async Task<IEnumerable<PaymentMethod>> Handle(GetPaymentMethodsQuery request, CancellationToken cancellationToken)
+    {
+        return await _paymentMethodService.GetPaymentMethodsAsync(request.UserId);
+    }
+}
