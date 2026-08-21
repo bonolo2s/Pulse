4183c257eca944d527be544cc8f729c2d1237040
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 18:37:00 +0200

    Payment response

---
+++ Pulse.Billing/DTOs/PaymentMethodResponse.cs
@@ -0,0 +1,15 @@
+﻿using Pulse.Billing.Enums;
+
+namespace Pulse.Billing.DTOs;
+
+public class PaymentMethodResponse
+{
+    public Guid Id { get; set; }
+    public PaymentMethodType Type { get; set; }
+    public CardBrand? Brand { get; set; }
+    public string? Last4 { get; set; }
+    public int? ExpiryMonth { get; set; }
+    public int? ExpiryYear { get; set; }
+    public string? BankName { get; set; }
+    public bool IsDefault { get; set; }
+}
