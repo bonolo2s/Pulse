c8a776dbc0ebb2d1eb06b288b0c89778afff9113
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 19:01:36 +0200

    GetEndpointCountHandlerGetEndpointCountHandle

---
+++ Pulse.Monitoring/Handlers/GetEndpointCountHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Pulse.Monitoring.Interfaces;
+using Pulse.Monitoring.Queries;
+
+namespace Pulse.Monitoring.Handlers;
+
+public class GetEndpointCountHandler : IRequestHandler<GetEndpointCountQuery, int>
+{
+    private readonly IMonitoringService _monitoringService;
+
+    public GetEndpointCountHandler(IMonitoringService monitoringService)
+    {
+        _monitoringService = monitoringService;
+    }
+
+    public async Task<int> Handle(GetEndpointCountQuery request, CancellationToken cancellationToken)
+    {
+        return await _monitoringService.GetEndpointCountAsync(request.UserId);
+    }
+}
