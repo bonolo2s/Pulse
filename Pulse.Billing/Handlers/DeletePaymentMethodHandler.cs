aa19f599db9f18d66ab8313c24663f22e3c345f6
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-21 18:41:28 +0200

    DeletePaymentMethodHandler

---
+++ Pulse.Billing/Handlers/DeletePaymentMethodHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Pulse.Billing.Commands;
+using Pulse.Billing.Interfaces;
+
+namespace Pulse.Billing.Handlers;
+
+public class DeletePaymentMethodHandler : IRequestHandler<DeletePaymentMethodCommand>
+{
+    private readonly IPaymentMethodService _paymentMethodService;
+
+    public DeletePaymentMethodHandler(IPaymentMethodService paymentMethodService)
+    {
+        _paymentMethodService = paymentMethodService;
+    }
+
+    public async Task Handle(DeletePaymentMethodCommand request, CancellationToken cancellationToken)
+    {
+        await _paymentMethodService.DeletePaymentMethodAsync(request.Id);
+    }
+}
