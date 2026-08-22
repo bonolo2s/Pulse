a22ce27a050398fe6c80cdc4ce51f0d287f19284
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-23 01:14:09 +0200

    BillingEvent fix migration

---
+++ Pulse.Billing/Migrations/20260822231325_MakeProcessedNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pulse.Billing.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeProcessedNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Processed",
+                table: "BillingEvents",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Processed",
+                table: "BillingEvents",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+    }
+}
