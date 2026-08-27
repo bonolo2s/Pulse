e04f40a4466dac81892372a03dab23d964332132
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-27 20:35:31 +0200

    IUserLookupService

---
+++ Pulse.Shared/Interfaces/IUserLookupService.cs
@@ -0,0 +1,7 @@
+﻿namespace Pulse.Shared.Interfaces
+{
+    public interface IUserLookupService
+    {
+        Task<Dictionary<Guid, string>> GetEmailsByUserIdsAsync(IEnumerable<Guid> userIds);
+    }
+}
