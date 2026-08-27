b4f615465842337759aaf6b03246db87ee6cc605
Bonolo Selowa <bonololloyd003@gmail.com>
2026-08-27 20:36:03 +0200

    Impliment UserLookupService

--- Pulse.Identity/Services/UserLookupService.cs
+++ Pulse.Identity/Services/UserLookupService.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.EntityFrameworkCore;
+using Pulse.Identity.DataAccess;
+using Pulse.Shared.Interfaces;
 namespace Pulse.Identity.Services
 {
-    public class UserLookupService
+    public class UserLookupService : IUserLookupService
     {
+        private readonly IdentityDbContext _context;
+        public UserLookupService(IdentityDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Dictionary<Guid, string>> GetEmailsByUserIdsAsync(IEnumerable<Guid> userIds)
+        {
+            return await _context.Users
+                .Where(u => userIds.Contains(u.Id))
+                .ToDictionaryAsync(u => u.Id, u => u.Email);
+        }
     }
 }
