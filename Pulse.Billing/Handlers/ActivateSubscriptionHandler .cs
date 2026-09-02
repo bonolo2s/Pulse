29cd1285b89c1b3a18c10ff62bb445c719cf7adf
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-02 21:02:00 +0200

    ActivateSubscriptionHandler

---
+++ Pulse.Billing/Handlers/ActivateSubscriptionHandler .cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Pulse.Billing.Commands;
+using Pulse.Billing.Interfaces;
+
+namespace Pulse.Billing.Handlers;
+
+public class ActivateSubscriptionHandler : IRequestHandler<ActivateSubscriptionCommand>
+{
+    private readonly ISubscriptionService _subscriptionService;
+
+    public ActivateSubscriptionHandler(ISubscriptionService subscriptionService)
+    {
+        _subscriptionService = subscriptionService;
+    }
+
+    public async Task Handle(ActivateSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        await _subscriptionService.ActivateSubscriptionFromWebhookAsync(
+            request.Email, request.SubscriptionCode, request.EmailToken, request.CustomerCode);
+    }
+}
