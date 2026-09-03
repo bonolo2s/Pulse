b8348170741ad54b10f88c66c71ba9e0918e2444
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-03 20:40:02 +0200

    UpdateInvoiceFromWebhookHandler

---
+++ Pulse.Billing/Handlers/UpdateInvoiceFromWebhookHandler .cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Pulse.Billing.Commands;
+using Pulse.Billing.Interfaces;
+
+namespace Pulse.Billing.Handlers;
+
+public class UpdateInvoiceFromWebhookHandler : IRequestHandler<UpdateInvoiceFromWebhookCommand>
+{
+    private readonly IInvoiceService _invoiceService;
+
+    public UpdateInvoiceFromWebhookHandler(IInvoiceService invoiceService)
+    {
+        _invoiceService = invoiceService;
+    }
+
+    public async Task Handle(UpdateInvoiceFromWebhookCommand request, CancellationToken cancellationToken)
+    {
+        await _invoiceService.UpdateInvoiceFromWebhookAsync(request.InvoiceCode, request.Status, request.Paid);
+    }
+}
