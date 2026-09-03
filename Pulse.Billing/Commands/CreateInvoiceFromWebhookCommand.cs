fa8c6ce9e67f924190c976789f5c969c3bcd011f
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-03 19:23:33 +0200

    CreateInvoiceFromWebhookCommand'

---
+++ Pulse.Billing/Commands/CreateInvoiceFromWebhookCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Pulse.Billing.Commands;
+
+public record CreateInvoiceFromWebhookCommand(
+    string SubscriptionCode,
+    string InvoiceCode,
+    int Amount,
+    string Currency,
+    string Status,
+    bool Paid,
+    DateTime? PaidAt) : IRequest;
