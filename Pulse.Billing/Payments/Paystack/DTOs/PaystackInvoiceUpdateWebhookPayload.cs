da4a7fb67645ea410560fcd088ec5d70d4cd8e4f
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-03 20:37:36 +0200

    PaystackInvoiceUpdateWebhookPayload

---
+++ Pulse.Billing/Payments/Paystack/DTOs/PaystackInvoiceUpdateWebhookPayload.cs
@@ -0,0 +1,17 @@
+﻿using Pulse.Billing.Payments.Paystack.DTOs;
+
+public class PaystackInvoiceUpdateWebhookPayload
+{
+    public string Event { get; set; } = string.Empty;
+    public PaystackInvoiceUpdateWebhookData Data { get; set; } = null!;
+}
+
+public class PaystackInvoiceUpdateWebhookData
+{
+    public string InvoiceCode { get; set; } = string.Empty;
+    public int Amount { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public bool Paid { get; set; }
+    public string Currency { get; set; } = string.Empty;
+    public PaystackWebhookCustomer Customer { get; set; } = null!;
+}
