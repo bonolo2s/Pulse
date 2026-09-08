7e87acd5809a8751e28b86a29adebf1283a3f1d0
Bonolo Selowa <bonololloyd003@gmail.com>
2026-09-08 18:39:03 +0200

    invioceType migration

---
+++ Pulse.Billing/Migrations/20260908163819_AddInvoiceTypeConversion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pulse.Billing.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoiceTypeConversion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Invoices",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Invoices",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
